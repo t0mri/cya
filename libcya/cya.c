9253092e5ae8bab507eba2b835aab9740a569000
t0mri <dat0mri@proton.me>
2024-03-11 21:40:25 +0530

    refactor: improve memory handling & readability
    - return a new copy of data instead of a pointer to `chunk.response` for safer
    data handling.
    - move all variables into one area for better maintainability.
    - rename variables for clarity and readability.

--- libcya/cya.c
+++ libcya/cya.c
@@ -13,7 +13,8 @@ struct memory {
   size_t size;
 };
 
-static size_t cb(void *data, size_t size, size_t nmemb, void *clientp) {
+static size_t write_function(void *data, size_t size, size_t nmemb,
+                             void *clientp) {
   size_t realsize = size * nmemb;
   struct memory *mem = (struct memory *)clientp;
 
@@ -32,14 +33,18 @@ static size_t cb(void *data, size_t size, size_t nmemb, void *clientp) {
 char *data;
 
 char *cya_search(char *query) {
-  char *baseURL = "https://www.youtube.com/results?search_query=", *url,
-       *substringStartDelimiter = "itemSectionRenderer\":",
-       *substringEndDelimiter = "},{\"continuationItemRenderer";
+  char *baseURL = "https://www.youtube.com/results?search_query=",
+       *encoded_query, *url,
+       *start_delimiter_substring = "itemSectionRenderer\":",
+       *end_delimiter_substring = "},{\"continuationItemRenderer",
+       *data_extract;
+  size_t data_extract_length;
 
-  data = "";
+  CURLcode response_status_code;
+  CURL *curl;
   struct memory chunk = {0};
 
-  CURL *curl = curl_easy_init();
+  curl = curl_easy_init();
   if (!curl) {
     fprintf(stderr, "Error: Unable to initiate cURL\n");
     exit(EXIT_FAILURE_CURL_INIT);
@@ -52,47 +57,55 @@ char *cya_search(char *query) {
     exit(EXIT_FAILURE_TOO_FEW_ARGUMENTS);
   }
 
-  char *escaped_query = curl_easy_escape(curl, query, strlen(query));
-  url = malloc(strlen(baseURL) + strlen(escaped_query) + 1);
+  encoded_query = curl_easy_escape(curl, query, strlen(query));
+  url = malloc(strlen(baseURL) + strlen(encoded_query) + 1);
   if (!url) {
     fprintf(stderr, "Error: Failed to allocate memory for URL\n");
     curl_easy_cleanup(curl);
     exit(EXIT_FAILURE_MEMORY_ALLOCATION);
   }
   strcpy(url, baseURL);
-  strcat(url, escaped_query);
+  strcat(url, encoded_query);
 
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_function);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
   curl_easy_setopt(curl, CURLOPT_URL, url);
 
-  CURLcode statusCode = curl_easy_perform(curl);
-  if (statusCode != CURLE_OK) {
+  response_status_code = curl_easy_perform(curl);
+  if (response_status_code != CURLE_OK) {
     fprintf(stderr, "Error: Unable to search the video\n");
     curl_easy_cleanup(curl);
     exit(EXIT_FAILURE_FETCH_DATA);
   }
 
-  data = strstr(chunk.response, substringStartDelimiter);
-  if (!data) {
+  data_extract = strstr(chunk.response, start_delimiter_substring);
+  if (!data_extract) {
     fprintf(stderr, "Error: Something went wrong on extracting data\n");
   }
-  *data = '\0';
-  data += strlen(substringStartDelimiter);
-  char *tempData = strstr(data, substringEndDelimiter);
-  if (tempData) {
-    *tempData = '\0';
+  data_extract += strlen(start_delimiter_substring);
+  *(strstr(data_extract, end_delimiter_substring)) = '\0';
+
+  data_extract_length = strlen(data_extract);
+  data = malloc(data_extract_length + 1);
+  if (!data) {
+    fprintf(stderr, "Error: Failed to allocate memory for data copy\n");
+    free(url);
+    free(chunk.response);
+    curl_easy_cleanup(curl);
+    curl_free(encoded_query);
+    exit(EXIT_FAILURE_MEMORY_ALLOCATION);
   }
+  strcpy(data, data_extract);
 
   free(url);
   free(chunk.response);
   curl_easy_cleanup(curl);
-  curl_free(escaped_query);
+  curl_free(encoded_query);
   return data;
 }
 
-void cya_save_json(char *fileName) {
-  FILE *file = fopen(fileName, "w");
+void cya_save_json(char *file_name) {
+  FILE *file = fopen(file_name, "w");
   fputs(data, file);
   fclose(file);
 }
